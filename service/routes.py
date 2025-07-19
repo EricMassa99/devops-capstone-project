bf0312a70756c045bdd879b914cec0564c42ca7b
EricMassa99 <eric.massa123@gmail.com>
2025-07-19 21:51:42 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,7 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-@app.route("/accounts", methods=["GET"])
+@app.route("/accounts", methods=["GET"]) 
 def list_accounts():
     """
     List all Accounts
@@ -77,7 +77,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-@app.route("/accounts/<int:account_id>", methods=["GET"])
+@app.route("/accounts/<int:account_id>", methods=["GET"]) 
 def get_accounts(account_id):
     """
     Reads an Account
@@ -93,7 +93,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-@app.route("/accounts/<int:account_id>", methods=["PUT"])
+@app.route("/accounts/<int:account_id>", methods=["PUT"]) 
 def update_accounts(account_id):
     """
     Update an Account
@@ -113,7 +113,7 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+@app.route("/accounts/<int:account_id>", methods=["DELETE"]) 
 def delete_accounts(account_id):
     """
     Delete an Account
